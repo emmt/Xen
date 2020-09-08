3da8e1d811e4809d76ae029c576cecb8fae406d1
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-09-08 16:06:04 +0200

    Add minimal server example

---
+++ tcltk/server.tcl
@@ -0,0 +1,10 @@
+#!/bin/sh
+# -*- tcl -*-
+#
+# The next line is executed by /bin/sh, but not tcl \
+exec tclsh "$0" ${1+"$@"}
+
+source [file join [file dirname [info script]] xen.tcl]
+
+set srv [::xen::server]
+vwait done
